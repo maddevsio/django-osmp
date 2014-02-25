328800a427241003ad0c63c16b921528fda175a5
Gen1us2k <minkin.andrew@gmail.com>
2014-02-25 15:29:19 +0600

    Changed txn_id field

---
+++ osmp/migrations/0005_auto__chg_field_osmp_txn_id.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'OSMP.txn_id'
+        db.alter_column('osmp', 'txn_id', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255))
+
+    def backwards(self, orm):
+
+        # Changing field 'OSMP.txn_id'
+        db.alter_column('osmp', 'txn_id', self.gf('django.db.models.fields.PositiveIntegerField')(unique=True))
+
+    models = {
+        u'osmp.osmp': {
+            'Meta': {'object_name': 'OSMP', 'db_table': "'osmp'"},
+            'account': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'added': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'money': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '7', 'decimal_places': '2'}),
+            'txn_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'txn_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['osmp']
